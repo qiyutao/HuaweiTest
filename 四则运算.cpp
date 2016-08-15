b8bf3c12111ee744461aef10a633e6e5cfa14564
qiyutao <qiyutao12@163.com>
2016-08-15 15:59:13 +0800

    Create 四则运算.cpp
    描述 	
    
    请实现如下接口
    
        /* 功能：四则运算
    
         * 输入：strExpression：字符串格式的算术表达式，如: "3+2*{1+2*[-4/(8-6)+7]}"
    
             * 返回：算术表达式的计算结果
    
         */
    
        public static int calculate(String strExpression)
    
        {
    
            /* 请实现*/
    
            return 0;
    
        } 
    
    约束：
    
        pucExpression字符串中的有效字符包括[‘0’-‘9’],‘+’,‘-’, ‘*’,‘/’ ,‘(’， ‘)’,‘[’, ‘]’,‘{’ ,‘}’。
    
        pucExpression算术表达式的有效性由调用者保证; 
    
     
    
    
    知识点 	栈
    运行时间限制 	10M
    内存限制 	128
    输入 	
    
    输入一个算术表达式
    输出 	
    
    得到计算结果
    样例输入 	3+2*{1+2*[-4/(8-6)+7]}
    样例输出 	25

---
+++ 四则运算.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int getNum(int &i,string sub) {
+    int old = i;
+    for(;i<sub.size();i++) {
+        if(sub.at(i)>'9'||sub.at(i)<'0') {
+            return atoi(sub.substr(old,i).c_str());
+        }
+    }
+    return atoi(sub.substr(old,i).c_str());
+}
+
+int above(char c) {
+    if(c=='+'||c=='-')
+        return 0;
+    if(c=='*'||c=='/')
+        return 1;
+    if(c=='{')
+        return 2;
+    if(c=='[')
+        return 3;
+    if(c=='(')
+        return 4;
+    return 0;
+}
+
+int cal(int n2,int n1,char opt) {
+    switch (opt) {
+        case '+':
+            return n1+n2;
+        case '-':
+            return n1-n2;
+        case '*':
+            return n1*n2;
+        case '/':
+            return n1/n2;
+    }
+    return 0;
+}
+
+int main() {
+    string expr;
+    cin>>expr;
+    stack<char> opt;
+    stack<int> num;
+
+    char old = '+';
+
+    int i=0;
+    while(i<expr.size()) {
+        if(expr.at(i)>='0'&&expr.at(i)<='9') {
+            num.push(getNum(i,expr));
+            old = '0';
+        } else if((old=='+'||old=='-'||old=='*'||old=='/'
+                 ||old=='{'||old=='['||old=='(' )&&expr.at(i)=='-') {
+            i++;
+            num.push(-1*getNum(i,expr));
+            old = '0';
+        } else {
+            if(opt.empty())
+                opt.push(expr.at(i));
+            else {
+                if(expr.at(i)=='}'||expr.at(i)==']'||expr.at(i)==')') {
+                    while(opt.top()!='{'&&opt.top()!='['&&opt.top()!='(') {
+                        int n1 = num.top();
+                        num.pop();
+                        int n2 = num.top();
+                        num.pop();
+                        char c = opt.top();
+                        opt.pop();
+                        num.push(cal(n1, n2, c));
+                    }
+                    opt.pop();
+                }
+                else {
+                    if(above(expr.at(i))>above(opt.top())||above(opt.top())>1)
+                        opt.push(expr.at(i));
+                    else {
+                        bool f = true;
+                        while(!opt.empty())
+                        {
+                            if(above(expr.at(i))<=above(opt.top())&&above(opt.top())<=1) {
+                                int n1 = num.top();
+                                num.pop();
+                                int n2 = num.top();
+                                num.pop();
+                                char c = opt.top();
+                                opt.pop();
+
+                                num.push(cal(n1, n2, c));
+                            }
+                            else {
+                                opt.push(expr.at(i));
+                                f= false;
+                                break;
+                            }
+                        }
+                        if(f)
+                            opt.push(expr.at(i));
+                    }
+                }
+            }
+            old = expr.at(i);
+            i++;
+
+        }
+    }
+
+    while(!opt.empty()) {
+        int n1 = num.top();
+        num.pop();
+        int n2 = num.top();
+        num.pop();
+        char c = opt.top();
+        opt.pop();
+        num.push(cal(n1,n2,c));
+    }
+
+    cout<<num.top();
+    return 0;
+}
